1ebfa8aaa426534cc457c53b3a10dde7ce26cd7b
Cayril <aymeric.bizouarn@hotmail.fr>
2019-06-13 12:00:39 +0200

    player correction

--- src/quoridor/Player.java
+++ src/quoridor/Player.java
@@ -2,65 +2,49 @@ package quoridor;
 
 /**
  * Player
- * @author
+ *
+ * @author Aymeric Bizouarn
  */
 public abstract class Player {
+    /**
+     * The player Name.
+     */
+    protected String name;
+    /**
+     * The player nbFences.
+     */
+    private int nbFences;
 
-	protected Board board;
-	protected Pawn pawn;
-	protected String name;
-	private int nbFences;
+    /**
+     * Player constructor
+     * it cans either be human or bot
+     * it has a dedidcated pawn and a number of fences left.
+     * @author Aymeric Bizouarn
+     */
+    public Player(String name) {
+        this.name = name;
+        this.nbFences = 10;
+    }
 
-	/**
-	 * Player constructor
-	 * it cans either be human or bot
-	 * it has a dedidcated pawn and a number of fences left.
-	 */
-	public Player() {
-		// TODO - implement Player.Player
-		throw new UnsupportedOperationException();
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public Pawn getPawn() {
-		return this.pawn;
-	}
+    /**
+     * Return number of player fence remaining.
+     * @return the number of fences left playable by the player
+     * @author Aymeric Bizouarn
+     */
+    public int checkNbRestingFences() {
+        return this.nbFences;
+    }
 
-	public String getName() {
-		return this.name;
-	}
-
-	/**
-	 * @return the number of fences left playable by the player
-	 * @author
-	 */
-	public int checkNbRestingFences() {
-		// TODO - implement Player.checkNbRestingFences
-		throw new UnsupportedOperationException();
-	}
-
-
-	public boolean checkHasFinished(){
-		// TODO - Player.checkHasFinished
-		throw new UnsupportedOperationException();
-	}
-
-	public void setNbFences(int nb) {
-		this.nbFences = nb;
-	}
-
-	/**
-	 *
-	 * @param square
-	 * @return
-	 */
-	public boolean checkFencePossible(Square square) {
-		// TODO - implement Player.checkFencePossible
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean checkExistingPath(){
-		// TODO - implement player.checkExistingPath
-		throw new UnsupportedOperationException();
-	}
-
+    /**
+     * Set Player nbFences.
+     * @param nbFences The player nbFences.
+     * @author Aymeric Bizouarn
+     */
+    public void setNbFences(int nbFences) {
+        this.nbFences = nbFences;
+    }
 }
--- src/test/TestAutoPlayer.java
+++ src/test/TestAutoPlayer.java
@@ -22,22 +22,4 @@ public class TestAutoPlayer {
         autoPlayer.setNbFences(0);
         assertEquals(autoPlayer.checkNbRestingFences(),0);
     }
-    @Test
-    public static void testCheckHasFinished(){
-        Game game = new Game();
-        assertTrue(game.getPlayer1().checkHasFinished());
-        game.getPlayer2().getPawn().setPosition(0,0);
-        assertFalse(game.getPlayer2().checkHasFinished());
-    }
-    @Test
-    public static void testCheckExistingPath(){
-        Game game = new Game();
-        assertTrue(game.getPlayer1().checkExistingPath());
-        game.getPlayer1().getPawn().setPosition(0,0);
-        game.getBoard().getGrid().get(0).setFenceN(true);
-        game.getBoard().getGrid().get(0).setFenceE(true);
-        game.getBoard().getGrid().get(0).setFenceS(true);
-        game.getBoard().getGrid().get(0).setFenceW(true);
-        assertFalse(game.getPlayer1().checkExistingPath());
-    }
 }

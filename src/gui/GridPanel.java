0d3f73cc75a3163c53b7ea11cc65e8a6071fb7de
PxRazhor <49936808+PxRazhor@users.noreply.github.com>
2019-06-17 09:12:12 +0200

    Update GridPanel.java

--- src/gui/GridPanel.java
+++ src/gui/GridPanel.java
@@ -1,4 +1,34 @@
 package gui;
 
-public class GridPanel {
+import quoridor.Board;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Center panel of the graphical representation. Contains the board and permits the player to play
+ * @author Pierre-Galaad 'P(x)' Naquet , Aymeric Bizouarn
+ */
+public class GridPanel extends JPanel {
+
+  JButton[][] grid = new JButton[9][9];
+  Board board;
+
+  public GridPanel(Board board) {
+    this.Board = board;
+
+    initComponent();
+  }
+
+  public void initComponent() {
+
+    getContentPane().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    getContentPane().setLayout(new gridLayout(9,9));
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        grid[i][j] = new JButton();
+      }
+    }
+  }
+
 }

1ae0779d5babf8caa80c7ea2ce735e5767ec61bc
Cayril <aymeric.bizouarn@hotmail.fr>
2019-06-17 11:04:45 +0200

    creation gui package

--- src/gui/GridPanel.java
+++ src/gui/GridPanel.java
@@ -29,11 +29,4 @@ public class GridPanel extends JPanel {
       }
     }
   }
-
-  public void refresh() {
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        grid[i][j] = new ImagePanel(this.board, i, j);
-      }
-  }
 }

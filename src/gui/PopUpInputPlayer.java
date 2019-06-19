8c7a6c355d8aa73bfeabef29a08cc94da861d939
PxRazhor <49936808+PxRazhor@users.noreply.github.com>
2019-06-19 10:32:02 +0200

    Input popups

---
+++ src/gui/PopUpInputPlayer.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Test {
+
+  public String popUpPlayer1() {
+    String m = JOptionPane.showInputDialog("Type player 1 name, use 'auto' to have a bot");
+    return m;
+  }
+
+  public String popUpPlayer2() {
+    String m = JOptionPane.showInputDialog("Type player 2 name, use 'auto' to have a bot");
+    return m;
+  }
+
+
+}
